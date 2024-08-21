c1f67ae842606cb87c9cb38639adb434c8dce15f
Bryanlover1 <165657669+Bryanlover1@users.noreply.github.com>
2024-08-21 08:24:41 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,79 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+
+global.owner = [
+  ['233530729233', 'BRYANT TECH', true], 
+  ['233263176982', 'BRYAN TECH', true],
+  ['233538733413', 'Bryanlover1', true], 
+  ['233508488192', 'Abraham Dwamena', true]
+] //Number of owners
+
+global.mods = ['233263176982', '233530729233', '233538733413', '233508488192'] 
+global.prems = ['233263176982', '233538733413', '233530729233', '233508488192']
+global.allowed = ['233530729233', '233538733413', '233263176982']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())]
+global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())]
+global.lolkeysapi = ['GataDios']
+global.beta = 'mLxstUwm'
+
+global.APIs = { // API Prefix
+  xteam: 'https://api.xteam.xyz', 
+  dzx: 'https://api.dhamzxploit.my.id',
+  lol: 'https://api.lolhuman.xyz',
+  violetics: 'https://violetics.pw',
+  neoxr: 'https://api.neoxr.my.id',
+  zenzapis: 'https://zenzapis.xyz',
+  akuari: 'https://api.akuari.my.id',
+  akuari2: 'https://apimu.my.id',
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api-fgmods.ddns.net'
+}
+
+global.APIKeys = { // APIKey Here
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': global.keysneoxr,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': global.keysxxx, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
+global.botname = 'DAZAI-V2'
+global.premium = 'true'
+global.packname = 'DAZAI TEAM' 
+global.author = '@DAZAI-V2' 
+global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
+global.igfg = '▢✓ Follow Our channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
+global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
+global.fgsc = 'https://github.com/Bryanlover1/DAZAI-V2'
+global.fglog = 'DAZAI.jpg' 
+global.thumb = fs.readFileSync('./DAZAI.jpg')
+
+global.wait = '*🕣 _DAZAI IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✔️'
+global.error = '❌' 
+global.xmoji = '🔥' 
+
+global.multiplier = 69 
+global.maxwarn = '3' // maximum warnings
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
+
+
